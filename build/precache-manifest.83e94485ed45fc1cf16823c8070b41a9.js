d9e87704ef91faf6a6a4c7a645cf916d6efa3413
ndungimark <ndungimark@gmail.com>
2019-10-27 22:05:33 +0300

    Add files via upload

---
+++ build/precache-manifest.83e94485ed45fc1cf16823c8070b41a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74be0844a14586674c3bf726f99042ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04325ed3969bf12a6055",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "947abe3e4a62357c714a",
+    "url": "/static/js/2.017bb613.chunk.js"
+  },
+  {
+    "revision": "04325ed3969bf12a6055",
+    "url": "/static/js/main.7616ef78.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
